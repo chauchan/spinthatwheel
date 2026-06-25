e071d4a63018e79c3c2c57cb32df0a66631bdc8a
Ricardo_Parra <rikiseba@gmail.com>
2026-06-25 19:56:22 -0400

    la rareza ahora es toggleable al crear cartas, las soporte por ahora no tienen rareza

--- scripts/card_db.gd
+++ scripts/card_db.gd
@@ -21,25 +21,26 @@ static func get_starter_misc() -> Array[CardData]:
 
 static func get_starter_support() -> Array[CardData]:
 	var cards: Array[CardData] = []
-	cards.append(_make("Company Policy", CardData.CardType.SUPPORT, 1, 0, 0, "Takes -2 damage next time you get berated"))
-	cards.append(_make("Gossip", CardData.CardType.SUPPORT, 1, 0, 0, "You know what they did. -1 ATK to one of their cards"))
-	cards.append(_make("Day Off", CardData.CardType.SUPPORT, 2, 0, 0, "Finally, you get some rest. Gets 3 HP"))
-	cards.append(_make("Transfer Request", CardData.CardType.SUPPORT, 2, 0, 0, "Trade a card on your field for one on your opponent's field"))
-	cards.append(_make("I QUIT", CardData.CardType.SUPPORT, 0, 0, 0, "Sacrifice one card from your field: deal 3 damage directly"))
-	cards.append(_make("9 to 5", CardData.CardType.SUPPORT, 0, 0, 0, "+2 energy"))
-	cards.append(_make("Productivity Bonus", CardData.CardType.SUPPORT, 1, 0, 0, "+1 energy, draw from attack deck"))
-	cards.append(_make("Loan", CardData.CardType.SUPPORT, 0, 0, 0, "+3 energy, -1 energy next 2 turns"))
-	cards.append(_make("Investing", CardData.CardType.SUPPORT, 0, 0, 0, "+1 energy this turn, +2 energy after 2 turns"))
-	cards.append(_make("Savings Account", CardData.CardType.SUPPORT, 0, 0, 0, "Gain energy for each card you have in your hand"))
+	cards.append(_make("Company Policy", CardData.CardType.SUPPORT, 1, 0, 0, "Takes -2 damage next time you get berated", false))
+	cards.append(_make("Gossip", CardData.CardType.SUPPORT, 1, 0, 0, "You know what they did. -1 ATK to one of their cards", false))
+	cards.append(_make("Day Off", CardData.CardType.SUPPORT, 2, 0, 0, "Finally, you get some rest. Gets 3 HP", false))
+	cards.append(_make("Transfer Request", CardData.CardType.SUPPORT, 2, 0, 0, "Trade a card on your field for one on your opponent's field", false))
+	cards.append(_make("I QUIT", CardData.CardType.SUPPORT, 0, 0, 0, "Sacrifice one card from your field: deal 3 damage directly", false))
+	cards.append(_make("9 to 5", CardData.CardType.SUPPORT, 0, 0, 0, "+2 energy", false))
+	cards.append(_make("Productivity Bonus", CardData.CardType.SUPPORT, 1, 0, 0, "+1 energy, draw from attack deck", false))
+	cards.append(_make("Loan", CardData.CardType.SUPPORT, 0, 0, 0, "+3 energy, -1 energy next 2 turns", false))
+	cards.append(_make("Investing", CardData.CardType.SUPPORT, 0, 0, 0, "+1 energy this turn, +2 energy after 2 turns", false))
+	cards.append(_make("Savings Account", CardData.CardType.SUPPORT, 0, 0, 0, "Gain energy for each card you have in your hand", false))
 	return cards
 
 
-static func _make(n: String, t: CardData.CardType, c: int, p: int, h: int, d: String) -> CardData:
-	var card := CardData.new()
-	card.card_name = n
-	card.type = t
-	card.cost = c
-	card.power = p
-	card.health = h
-	card.description = d
-	return card
+static func _make(n: String, t: CardData.CardType, c: int, p: int, h: int, d: String, ar: bool = true) -> CardData:
+    var card := CardData.new()
+    card.card_name = n
+    card.type = t
+    card.cost = c
+    card.power = p
+    card.health = h
+    card.description = d
+    card.applies_rarity = ar
+    return card
