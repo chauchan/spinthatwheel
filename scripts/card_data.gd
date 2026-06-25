8fc51a5ba5f2790713beb2e532b420fe05181dd8
Ricardo_Parra <rikiseba@gmail.com>
2026-06-25 19:51:40 -0400

    a;adi las rarezas

--- scripts/card_data.gd
+++ scripts/card_data.gd
@@ -2,6 +2,8 @@ extends Resource
 class_name CardData
 
 enum CardType { ATTACK, SUPPORT, TANK, MISC }
+enum Rarity { COMMON, UNCOMMON, RARE, EPIC, LEGENDARY }
+
 
 @export var card_name: String = ""
 @export var type: CardType = CardType.ATTACK
@@ -9,4 +11,16 @@ enum CardType { ATTACK, SUPPORT, TANK, MISC }
 @export var power: int = 0
 @export var health: int = 0
 @export var description: String = ""
-@export var rarity: String = ""
+@export var applies_rarity: bool = true
+@export var rarity: Rarity = Rarity.COMMON
+
+
+
+func get_rarity_multiplier() -> float:
+    match rarity:
+        Rarity.COMMON:    return 1.0
+        Rarity.UNCOMMON:  return 1.25
+        Rarity.RARE:      return 1.5
+        Rarity.EPIC:      return 2.0
+        Rarity.LEGENDARY: return 3.0
+    return 1.0
