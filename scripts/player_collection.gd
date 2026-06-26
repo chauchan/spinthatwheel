5829ea450e327e6aa560dba4b831e4581ce11378
Ricardo_Parra <rikiseba@gmail.com>
2026-06-25 22:54:08 -0400

    creamos el script para guardar los datos del jugador

---
+++ scripts/player_collection.gd
@@ -0,0 +1,10 @@
+extends Node
+
+extends Node
+
+const SAVE_PATH = "user://save_data.json"
+
+var lives: int = 3
+var current_floor: int = 1
+var collection: Array[CardData] = []
+var current_deck: Array[CardData] = []
